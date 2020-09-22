5c953d31df9b10440b76714c01a1e8f9f4e1d1f7
587906 <587906@stud.hvl.no>
2020-09-22 11:54:03 +0200

    Ferdig versjon

--- src/no/hvl/dat100/oppgåve5.java
+++ src/no/hvl/dat100/oppgåve5.java
@@ -2,9 +2,21 @@ package no.hvl.dat100;
 import static javax.swing.JOptionPane.*;
 public class oppgåve5 {
 	public static void main(String[] args) {
+		
+		for (int stud =1; stud<=10; stud++) {
+			
+			
+		
 		int poengsum = Integer.parseInt (showInputDialog ("Skriv inn poengsum"));
 		char karakter = ' ';
 		
+		
+		
+		while (poengsum <0 || poengsum >100) {
+			showMessageDialog (null, "Ugyldig poengsumm, kan berre være mellom 0-100" );
+			
+		poengsum = Integer.parseInt (showInputDialog ("Skriv inn gyldig poengsum"));
+		}
 		
 		if (poengsum <40) {
 			karakter = 'F';
@@ -22,9 +34,11 @@ public class oppgåve5 {
 		else if (poengsum <90) {
 			karakter = 'B';
 		}
-		else karakter = 'A';
+		else {
+			karakter = 'A';
+		}
 		
 		showMessageDialog (null, "Karakteren din er " + karakter);
 	}
-	
+	}
 }
